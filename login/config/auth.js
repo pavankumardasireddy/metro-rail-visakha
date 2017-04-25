0981143b2273e014851eb62fbe8a249ee5493a0b
pavankumardasireddy <pavankumardasireddy@gmail.com>
2017-04-25 12:52:13 +0530

    Signed-off-by: pavankumardasireddy <pavankumardasireddy@gmail.com>

---
+++ login/config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '280160645767758',
+		'clientSecret': '90d1133e33c4dede6cf9db0d2e040e9d',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': '464027654576-d5e75ac55b3e7mq66krm8qjo1aeni7oj.apps.googleusercontent.com',
+		'clientSecret': 'tvob5H2j2qxIvNqPRXI9QyWM',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
